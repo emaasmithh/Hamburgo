ab3e20429f1dbc9229fc8f1f9a7934a3c5aa5969
Emanuel Smith <emaasmithh@hotmail.com>
2024-06-18 20:56:20 -0300

    feat: producto list

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import index
+from .views import ProductoList
 
 app_name = "producto"
 
 urlpatterns = [
-    path("", index, name="index"),    
+    path("", index, name="index"),
+    path("list/", ProductoList.as_view(), name="producto_list"),    
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Producto
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 def index(request):
     return render(request, "producto/index.html")
+
+class ProductoList(ListView):
+    model = Producto
