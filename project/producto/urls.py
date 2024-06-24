458d4191a3e3be30e481276a37b9744825169113
Emanuel Smith <emaasmithh@hotmail.com>
2024-06-24 19:25:27 -0300

    feat: cambios en producto

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import index
-from .views import ProductoList, agregar_producto
+from .views import ProductoList, agregar_producto, mostrar_detalle_producto, ProductoDetail, ProductoUpdate
 
 
 app_name = "producto"
@@ -8,5 +8,7 @@ app_name = "producto"
 urlpatterns = [
     path("", index, name="index"),
     path("list/", ProductoList.as_view(), name="producto_list"),  
-    path("agregar_producto/<int:producto_id>", agregar_producto, name="agregar_producto")      
+    path("agregar_producto/<int:producto_id>", agregar_producto, name="agregar_producto"),
+    path("detail/<int:pk>", ProductoDetail.as_view(), name="detalle_producto"),
+    path("update/<int:pk>", ProductoUpdate.as_view(), name="producto_update"),     
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Producto
+from .forms import ProductoForm
 from venta.models import Carrito
 
 from django.urls import reverse_lazy
@@ -23,4 +24,15 @@ def agregar_producto(request, producto_id):
     carrito.agregar(producto)
     return redirect("producto:producto_list")
 
+def mostrar_detalle_producto(request, producto_id):
+    producto = Producto.objects.get(id=producto_id)
+    context = {"producto": producto}
+    return render(request, "detalle_producto.html", context)
 
+class ProductoDetail(DetailView):
+    model = Producto
+
+class ProductoUpdate(UpdateView):
+    model = Producto
+    form_class = ProductoForm
+    success_url = reverse_lazy("producto:producto_list")    
